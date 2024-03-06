0dbbbea49b15671c8908127ea704ceff5686d8e6
Zach Camara <zachary.camara@hpe.com>
2024-03-06 16:39:37 -0500

    docs: :memo: formatting prep for 9.3 compatibility

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 sys.path.insert(0, os.path.abspath("../../pyedgeconnect/orch/"))
 sys.path.insert(0, os.path.abspath("../../pyedgeconnect/ecos/"))
 
-# -- Project information -----------------------------------------------------
+# -- Project information ----------------------------------------------
 
 project = "pyedgeconnect"
 copyright = "2022 Hewlett Packard Enterprise Development LP"
@@ -19,7 +19,7 @@ version = "0.15"
 release = "0.15.4-a1"
 
 
-# -- General configuration ---------------------------------------------------
+# -- General configuration --------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -40,18 +40,19 @@ templates_path = ["_templates"]
 exclude_patterns = []
 
 
-# -- Options for HTML output -------------------------------------------------
+# -- Options for HTML output ------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
+# The theme to use for HTML and HTML Help pages.  See the documentation
+# for a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
 html_logo = "_static/hpe_aruba_black_pos_rgb.png"
 
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+# Add any paths that contain custom static files (such as style sheets)
+# here, relative to this directory. They are copied after the builtin
+# static files, so a file named "default.css" will overwrite the
+# builtin "default.css".
 html_static_path = ["_static"]
 
 
